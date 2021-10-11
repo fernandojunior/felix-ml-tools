b3a2c72727a856edbedd0aa4bd50b2dc07976437
Fernando Felix <fernandofelix@copin.ufcg.edu.br>
2021-10-11 01:40:46 -0300

    config and apply flake8 linter

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
-import os
-from os import path
 
 # Model package name
-NAME = 'felix_ml_tools'
+NAME = "felix_ml_tools"
 
 # Dependecies for the package
-with open('requirements.txt') as r:
+with open("requirements.txt") as r:
     DEPENDENCIES = [
-        dep for dep in map(str.strip, r.readlines())
-        if all([not dep.startswith("#"),
-                not dep.endswith("#dev"),
-                len(dep) > 0])
+        dep
+        for dep in map(str.strip, r.readlines())
+        if all([not dep.startswith("#"), len(dep) > 0])
     ]
 
 # Project descrpition
@@ -21,11 +18,11 @@ with open("README.md") as f:
 
 setup(
     name=NAME,
-    version='0.0.1',
-    description='A ML/EDA lib to rule any problem.',
+    version="0.0.1",
+    description="A ML/EDA lib to rule any problem.",
     long_description=LONG_DESCRIPTION,
-    author='Fernando Felix',
+    author="Fernando Felix",
     packages=find_packages(exclude=("tests", "docs")),
     # external packages as dependencies
-    install_requires=DEPENDENCIES
+    install_requires=DEPENDENCIES,
 )
